c63e8cd77bd146bb2e6fdeb24bf5c21d036e570a
epimore <kz986542@gmail.com>
2024-10-29 15:42:17 +0800

    日志支持：添加使用说明

--- common/src/logger.rs
+++ common/src/logger.rs
@@ -6,22 +6,35 @@ use yaml_rust::Yaml;
 use cfg::{conf, Conf};
 use serde::{Deserialize, Deserializer};
 
+/// 通过配置文件控制日志格式化输出
+/// # Examples
 ///
+///  ```yaml
 /// log:
-//   level: debug #默认日志等级
-//   prefix: server #默认日志文件前缀; eg:server_2024-10-26.log
-//   store_path: ./logs
-//   specify: #指定日志输出
-//     - crate_name: hyper::http::h1  #或者hyper用指全部
-//       level: Info #日志等级
-//       file_name_prefix: hyper #日志文件前缀
-//       additivity: false #是否记录到其父日志文件中
+///   level: warn #全局日志等级 可选：默认 info
+///   prefix: server #全局日志文件前缀; 可选：默认 app 指定生成日志文件添加日期后缀，如 server_2024-10-26.log
+///   store_path: ./logs #日志文件根目录；可选 默认 当前目录
+///   specify: #指定日志输出 可选，不指定日志
+///     - crate_name: test_log::a  #或者test_log用指全部  必选
+///       level: debug #日志等级 必选
+///       file_name_prefix: a #日志文件前缀 可选 当未指定时，记录到全局日志文件中，等级由指定日志等级控制
+///       additivity: false #是否记录到全局日志文件中 可选 默认false,全局日志文件会再次根据全局日志等级过滤记录
+///     - crate_name: test_log::b  #或者test_log用指全部
+///       level: debug #日志等级
+///     - crate_name: test_log::c  #或者test_log用指全部
+///       level: debug #日志等级
+///       file_name_prefix: c #日志文件前缀
+///       additivity: true #是否记录到全局日志文件中
+///  ```
 #[derive(Debug, Deserialize)]
 #[conf(prefix = "log")]
 pub struct Logger {
+    #[serde(default)]
     store_path: PathBuf,
+    #[serde(default = "default_app")]
     prefix: String,
     #[serde(deserialize_with = "validate_level")]
+    #[serde(default = "default_info")]
     level: String,
     specify: Option<Vec<Specify>>,
 }
@@ -72,11 +85,10 @@ impl Logger {
                     .filter(move |metadata| metadata.target().starts_with(&target));
 
                 // 如果指定了文件名前缀，则将日志输出到指定的文件
-                if let Some(prefix) = &s.file_name_prefix {
-                    module_dispatch = module_dispatch
-                        .chain(std::io::stdout())
-                        .chain(fern::DateBased::new(&log.store_path, format!("{}_{}.log", prefix, "%Y-%m-%d".to_string())));
-                }
+                let prefix = s.file_name_prefix.as_deref().unwrap_or(&log.prefix);
+                module_dispatch = module_dispatch
+                    .chain(std::io::stdout())
+                    .chain(fern::DateBased::new(&log.store_path, format!("{}_{}.log", prefix, "%Y-%m-%d".to_string())));
 
                 dispatch = dispatch.chain(module_dispatch);
             }
@@ -118,6 +130,14 @@ where
     Ok(level)
 }
 
+fn default_info() -> String {
+    "info".to_string()
+}
+
+fn default_app() -> String {
+    "app".to_string()
+}
+
 #[cfg(test)]
 mod tests {
     use cfg::{Conf};
@@ -140,5 +160,10 @@ mod tests {
         // 指定模块日志
         log::info!(target: "hyper::http::h1", "This is a hyper::http::h1 log message.");
         log::debug!(target: "hyper", "This is a hyper debug log message.");
+    }
+
+    #[test]
+    fn test_default_value() {
+        println!("{:?}", PathBuf::default());
     }
 }
