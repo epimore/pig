8cf5cf26b000cd9feb3258bb1ffa4a189b186602
epimore <kz986542@gmail.com>
2024-11-21 23:37:37 +0800

    添加守护进程：日志输出

--- common/src/daemon/mod.rs
+++ common/src/daemon/mod.rs
@@ -1,12 +1,15 @@
 use std::{env, thread};
-use std::fs::{File, OpenOptions};
-use std::io::Read;
-use std::process::{Command};
+use std::fs::{File};
+use std::io::{BufRead, BufReader, Read};
+use std::os::fd::FromRawFd;
+use std::process::{Command, exit};
 use std::time::Duration;
 
-use daemonize::{Daemonize};
+use daemonize::{Daemonize, Outcome};
+use nix::unistd::{close, dup2};
 
-use exception::{GlobalResult};
+use exception::{GlobalError, GlobalResult};
+use exception::anyhow::anyhow;
 
 pub trait Daemon<T> {
     fn init_privilege() -> GlobalResult<(Self, T)>
@@ -49,31 +52,88 @@ where
 {
     let exe_path = env::current_exe().expect("Failed to get current executable path");
     let wd = exe_path.parent().expect("invalid path");
-    let out_path = exe_path.with_extension("err");
-    let stderr = OpenOptions::new().create(true).write(true).append(true).open(out_path).expect("Failed to open run.out");
     // 获取当前用户和组的 ID
     let uid = users::get_current_uid();
     let gid = users::get_current_gid();
 
+    // 创建管道
+    let mut pipe_fds = [0; 2];
+    unsafe {
+        if libc::pipe(pipe_fds.as_mut_ptr()) < 0 {
+            eprintln!("Failed to create pipe");
+            exit(1);
+        }
+    }
+
+    let reader_fd = pipe_fds[0];
+    let writer_fd = pipe_fds[1];
+
+    //在 Unix 系统中，fork() 调用会复制当前进程的资源，包括代码、内存、文件描述符等。父子进程在 fork() 后共享代码，但会根据 fork() 的返回值进入不同的逻辑分支：
     let daemonize = Daemonize::new()
         .pid_file(exe_path.with_extension("pid"))
         .chown_pid_file(true)
         .working_directory(wd)
         .user(uid) // 设置用户权限
         .group(gid)
-        .privileged_action(|| D::init_privilege())
-        // .stdout(stdout.try_clone().expect("Failed to clone start log file handle"))  // 重定向 stdout 到文件
-        .stderr(stderr); // 重定向 stdout 到文件
-    match daemonize.start() {
-        Ok(Ok((d, t))) => {
-            eprintln!("Service started successfully...");
-            d.run_app(t).unwrap()
+        .privileged_action(move || {
+            // 将管道写端重定向到 stdout 和 stderr
+            dup2(writer_fd, libc::STDOUT_FILENO).expect("Failed to redirect stdout");
+            dup2(writer_fd, libc::STDERR_FILENO).expect("Failed to redirect stderr");
+            close(writer_fd).expect("Failed to close writer_fd");
+            D::init_privilege()
+        });
+
+    let mut first_child_exit_code = 0;
+    let res = match daemonize.execute() {
+        Outcome::Child(Ok(child)) => {
+            Some(child.privileged_action_result)
         }
-        Ok(Err(e)) => {
-            eprintln!("Service started failed: {}.", e);
+        Outcome::Child(Err(err)) => {
+            eprintln!("Start middle process error: {}", err);
+            Some(Err(GlobalError::SysErr(anyhow!(err))))
         }
-        Err(e) => {
-            eprintln!("Error starting the service:{}", e);
+        Outcome::Parent(Err(err)) => {
+            eprintln!("Start main process error: {}", err);
+            Some(Err(GlobalError::SysErr(anyhow!(err))))
+        }
+        Outcome::Parent(Ok(parent)) => {
+            first_child_exit_code = parent.first_child_exit_code;
+            println!("...");
+            None
+        }
+    };
+    match res {
+        None => {
+            // 关闭管道的写端
+            close(writer_fd).expect("Failed to close writer_fd in parent");
+            // 读取守护进程日志
+            read_pipe_logs(reader_fd);
+            exit(first_child_exit_code);
+        }
+        Some(Ok((d, t))) => {
+            match d.run_app(t) {
+                Ok(_) => {}
+                Err(e) => {
+                    eprintln!("App start run error: {}", e);
+                    exit(first_child_exit_code);
+                }
+            }
+        }
+        Some(Err(e)) => {
+            eprintln!("App init error: {}", e);
+            exit(first_child_exit_code);
+        }
+    }
+}
+
+// 管道日志读取函数
+fn read_pipe_logs(reader_fd: i32) {
+    let reader = unsafe { File::from_raw_fd(reader_fd) };
+    let reader = BufReader::new(reader);
+    for line in reader.lines() {
+        match line {
+            Ok(content) => println!("Daemon log: {}", content),
+            Err(e) => eprintln!("Error reading pipe: {}", e),
         }
     }
 }
@@ -82,13 +142,13 @@ fn stop_service() -> bool {
     let mut b = false;
     match read_pid() {
         None => {
-            eprintln!("Service is not running.");
+            eprintln!("Service is not running\n   ...failed");
         }
         Some(pid) => {
             if let Err(e) = send_terminate_signal(pid) {
                 eprintln!("Failed to stop the service: {}", e);
             } else {
-                eprintln!("Service stopped.");
+                eprintln!("stop...\n   ...success");
                 b = true;
             }
         }
@@ -100,8 +160,8 @@ fn restart_service<D, T>()
 where
     D: Daemon<T>,
 {
+    println!("restart ...");
     if stop_service() {
-        eprintln!("restart ...");
         thread::sleep(Duration::from_secs(1));
         start_service::<D, T>();
     }
